18c08e3bc51706d6edbd181212a30e477c559dc3
eric recke <ericrecke@hotmail.com>
2021-03-01 01:36:10 -0300

    0.4
    Act all

--- AmericaVirtualWS/AmericaVirtualWS/Global.asax.cs
+++ AmericaVirtualWS/AmericaVirtualWS/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.ServiceModel.Activation;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -13,8 +15,30 @@ namespace AmericaVirtualWS
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            //RouteTable.Routes.Add(new ServiceRoute("PlenarioServices", new WebServiceHostFactory(), typeof(AmericaVirtualService)));
             var currentConnection = ConfigurationManager.AppSettings["CurrentConnection"];
             string connStr = ConfigurationManager.ConnectionStrings[currentConnection].ConnectionString;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            string CustomerIP = HttpContext.Current.Request.UserHostAddress;
+            if (HttpContext.Current.Request.ServerVariables["HTTP_VIA"] != null)
+            {
+                CustomerIP = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+            }
+            else
+            {
+                CustomerIP = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"].ToString();
+            }
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
         }
 
         //protected void Session_Start(object sender, EventArgs e)
